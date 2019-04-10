0a65dc41022b5201feb5c8e27ae1c99582317854
Adriano <afcf@cin.ufpe.br>
2019-04-09 21:33:16 -0300

    visualizacao da lista de alunos cadastrados

--- gui/ta-gui/src/karma.conf.js
+++ gui/ta-gui/src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/ta-gui'),
+      dir: require('path').join(__dirname, '../coverage/ta-guo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
